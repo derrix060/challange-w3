8ea1a3466f717314375695ed016893110ef6a031
derrix060 <mariotapra@gmail.com>
2019-10-02 00:04:08 +1300

    Fix typo on web-app port

--- test.sh
+++ test.sh
@@ -17,14 +17,14 @@ function wait_for_webapp_start {
 wait_for_webapp_start
 
 # Check getting a wrong key
-[[ $(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:5001/key/wrong_key) -eq 404 ]]
+[[ $(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:5000/key/wrong_key) -eq 404 ]]
 
 # Test adding wrong value
-curl -v --silent -X PUT http://localhost:5001/key -H 'Content-Type: application/json' -d '{"wrong_key": "wrong_value"}' 2>&1 | grep "HTTP/1.0 500 INTERNAL SERVER ERROR"
+curl -v --silent -X PUT http://localhost:5000/key -H 'Content-Type: application/json' -d '{"wrong_key": "wrong_value"}' 2>&1 | grep "HTTP/1.0 500 INTERNAL SERVER ERROR"
 
 $ Test adding correct value
-[[ $(curl -X PUT http://localhost:5001/key -H 'Content-Type: application/json' -d '{"key": "key1", "value": "value2"}') == "Pair \"{'key': 'key1', 'value': 'value2'}\" saved." ]]
+[[ $(curl -X PUT http://localhost:5000/key -H 'Content-Type: application/json' -d '{"key": "key1", "value": "value2"}') == "Pair \"{'key': 'key1', 'value': 'value2'}\" saved." ]]
 
 # Test quering correct value
-[[ $(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:5001/key/key1) -eq 200 ]]
-[[ $(curl http://localhost:5001/key/key1) == '{"key": "key1", "value": "value2"}' ]]
+[[ $(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:5000/key/key1) -eq 200 ]]
+[[ $(curl http://localhost:5000/key/key1) == '{"key": "key1", "value": "value2"}' ]]
