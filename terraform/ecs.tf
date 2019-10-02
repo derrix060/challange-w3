7b20de44b861c64971f6bb8a0ef2bece6b946eb0
derrix060 <mariotapra@gmail.com>
2019-10-02 22:15:12 +1300

    Fix issue with task having less resources than combination of apps

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -50,8 +50,8 @@ resource "aws_ecs_task_definition" "app" {
   execution_role_arn       = aws_iam_role.app-service-role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 2048
+  memory                   = 4096
   container_definitions    = data.template_file.app.rendered
 }
 
