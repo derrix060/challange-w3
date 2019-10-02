35a601c53b862ad9ea410142ea3cfa9865565164
Mario Apra <mario.apra@soeidental.com>
2019-10-03 10:09:18 +1300

    Add dynamoDB table to be used on the state file

--- terraform/state_terraform.tf
+++ terraform/state_terraform.tf
@@ -16,3 +16,20 @@ resource "aws_s3_bucket" "mario-terraform-state-storage" {
       Name = "S3 Remote Terraform State Store"
     }
 }
+
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
